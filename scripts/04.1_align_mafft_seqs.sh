6ffad7a65a9bbd2ea54ab3b630c8bb04538586d7
NataKich <natastarosto4ka@gmail.com>
2025-06-01 01:13:46 +0300

    alignment with MAFFT

---
+++ scripts/04.1_align_mafft_seqs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Navigate to the target directory
+cd "../data/processed/03_aligned_seq/" || { echo "Error: Failed to navigate to directory"; exit 1; }
+
+# Perform a MAFFT alignment
+echo "Running MAFFT for file ..."
+mafft --localpair --thread 8 dataset_ABC_ATP_clu_and_AAA_ATP.fasta > dataset_ABC_ATP_clu_rep_localpair_mafft.fasta || { echo "Align error"; exit 1; }
+
+echo "Done! Align is complete."
