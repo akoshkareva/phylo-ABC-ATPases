e3e9bad558283e342f370ccd0ebf4fdecada28e2
NataKich <natastarosto4ka@gmail.com>
2025-06-01 01:38:07 +0300

    Align with muscle

--- scripts/04.2_align_muscle_seqs.sh
+++ scripts/04.2_align_muscle_seqs.sh
@@ -3,7 +3,7 @@
 cd "../data/processed/03_aligned_seq/" || { echo "Error: Failed to navigate to directory"; exit 1; }
 
 # Perform a MUSCLE alignment
-echo "Running MUSCLE for file ..."
+echo "Running MUSCLE for file dataset_ABC_ATP_with_AAA_ATP"
 muscle -super5 dataset_ABC_ATP_with_AAA_ATP.fasta -output dataset_ABC_ATP_muscle.fasta -threads 8 || { echo "Align error"; exit 1; }
 
 echo "Done! Align is complete."
