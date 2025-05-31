0c06c48b2fe88b2fd9d1b9f2dc0b3b5f4faf62f1
NataKich <natastarosto4ka@gmail.com>
2025-06-01 01:20:03 +0300

    Update 04.1_align_mafft_seqs.sh

--- scripts/04.1_align_mafft_seqs.sh
+++ scripts/04.1_align_mafft_seqs.sh
@@ -4,6 +4,6 @@ cd "../data/processed/03_aligned_seq/" || { echo "Error: Failed to navigate to d
 
 # Perform a MAFFT alignment
 echo "Running MAFFT for file ..."
-mafft --localpair --thread 8 dataset_ABC_ATP_clu_and_AAA_ATP.fasta > dataset_ABC_ATP_clu_rep_localpair_mafft.fasta || { echo "Align error"; exit 1; }
+mafft --localpair --thread 8 dataset_ABC_ATP_clu_and_AAA_ATP.fasta > dataset_ABC_ATP_localpair_mafft.fasta || { echo "Align error"; exit 1; }
 
 echo "Done! Align is complete."
