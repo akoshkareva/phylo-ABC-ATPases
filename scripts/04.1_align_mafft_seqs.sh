6de00ecc297289816c9864e748a3438e3f7319c6
NataKich <natastarosto4ka@gmail.com>
2025-06-01 02:17:19 +0300

    align with MAFFT

--- scripts/04.1_align_mafft_seqs.sh
+++ scripts/04.1_align_mafft_seqs.sh
@@ -3,7 +3,7 @@
 cd "../data/processed/03_aligned_seq/" || { echo "Error: Failed to navigate to directory"; exit 1; }
 
 # Perform a MAFFT alignment
-echo "Running MAFFT for file ..."
-mafft --localpair --thread 8 dataset_ABC_ATP_with_AAA_ATP.fasta > dataset_ABC_ATP_localpair_mafft.fasta || { echo "Align error"; exit 1; }
+echo "Running MAFFT for file dataset_ABC_ATP_clu_with_AAA_ATP.fasta"
+mafft --localpair --thread 8 dataset_ABC_ATP_clu_with_AAA_ATP.fasta > dataset_ABC_ATP_muscle.fasta || { echo "Align error"; exit 1; }
 
 echo "Done! Align is complete."
