294bb3e02d6a616b9b1c8bb4d678ea090aa1b5a4
NataKich <natastarosto4ka@gmail.com>
2025-06-01 00:17:11 +0300

    bash script for clustering

---
+++ scripts/03.1_cluster_seqs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Navigate to the target directory
+cd "../data/processed/02_clustered_seq/" || { echo "Error: Failed to navigate to directory"; exit 1; }
+
+# For exsisting results
+rm dataset_ABC_ATP_clu_rep.fasta
+
+# 1. Creating the MMseqs2 database
+echo "Step 1: Create a database..."
+mmseqs createdb dataset_ABC_ATP.faa dataset_ABC_ATP || { echo "Error during database creation"; exit 1; }
+
+# 2. Clustering with a minimum identity of 70% and with 70% coverage
+echo "Шаг 2: Step 2: Clustering of sequences..."
+mmseqs cluster dataset_ABC_ATP clu tmp --min-seq-id 0.7 --threads 8 || { echo "Clustering erro"; exit 1; }
+
+# 3. Extraction of cluster representatives
+echo "Step 3: Extracting Cluster Representatives..."
+mmseqs createsubdb clu dataset_ABC_ATP dataset_ABC_ATP_clu_rep || { echo "Error in extracting representatives"; exit 1; }
+
+echo "Done! All steps have been completed successfully."
+
+
